561394f78ec2e7a1f042d8c0db3db32a9a18137e
inclavus <inclavus@gmail,com>
2022-11-23 14:06:46 +0530

    updated index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { response } = require("express");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "ejs");
 
 var data = {
