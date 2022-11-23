7aeef70ebbf146f1a13919717f121ad424f1800c
inclavus <inclavus@gmail,com>
2022-11-23 14:15:01 +0530

    added view

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
 
 var data = {
